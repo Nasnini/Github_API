45f2145bada5cf65e65ea715c584d3d1ae688922
Nasnini <theo.kobuoe@umuzi.com>
2020-06-24 15:03:04 +0200

    Made changes to script

--- github_api.py
+++ github_api.py
@@ -1,21 +1,19 @@
+# import needed modules
 import requests
-import black
+#import black
 from datetime import date
 
-pull_requests = requests.get("https://api.github.com/repos/Umuzi-org/tech-department/pulls")
+#-------------------------------------------------------------------------
+# Function that retrieves pull request with - repository name, start date
+# and end date as arguments
+#-------------------------------------------------------------------------
+def github_pull(repo_name, start_date, end_date):
+    pull_requests = requests.get(f"https://api.github.com/repos/Umuzi-org/tech-department/pulls?id=37012297")
+    my_request = pull_requests.json()
+    return my_requests
 
-print(pull_requests.json)
-
-def pull_requests(owner, repo_name, start_date, end_date):
-    pull_requests = requests.get(f"https://api.github.com/repos/{owner}/{repo_name}/pulls?id=37012297")
-    return pull_requests
-
-repos = {
-    "id": 37012297,
-    "created_at": "2020-04-20T11:23:53Z",
-    "updated_at": "2020-04-20T11:23:53Z",
-}
-
-for key, value in repos.items():
-    if "2020-04-16T04:27:19Z" > pulls < "2020-04-20T11:23:53Z":
-        print(pulls)
+# repos = {
+#     "id": 37012297,
+#     "created_at": "2020-04-20T11:23:53Z",
+#     "updated_at": "2020-04-20T11:23:53Z",
+# }
